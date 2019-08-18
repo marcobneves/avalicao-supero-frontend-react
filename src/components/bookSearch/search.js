aae7942d80a2e077c077c55781da297db7f36a53
Marco Barroso <marcobneves@gmail.com>
2019-08-17 22:05:10 -0400

    Created Search List and Details components

---
+++ src/components/bookSearch/search.js
@@ -0,0 +1,37 @@
+
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            // titulo: '',
+            // preco: '',
+            // autor: '',
+            // autores: []
+        }
+    }
+
+    SearchRequest = () => {
+        // let url = "http://5d123a8084e9060014576aeb.mockapi.io/autor";
+        // fetch(url).then(resposta => {
+        //     return resposta.json();
+        // })
+        //     .then(dados => {
+        //         // console.log('DADOS', dados);
+        //         this.setState({ autores: dados })
+        //     })
+        //     .catch(e => {
+        //         console.log('ERRO AO BUSCAR DADOS')
+        //         this.setState({ error: true });
+        //     })
+    }
+
+    render() {
+        return (
+            <div>Search</div>
+        )
+    }
+
+}
